2b9488808bc6230d901b146944f0c793f263cfef
sakuhei <sakuhei@metropolia.fi>
2025-11-03 20:56:05 +0200

    test: add integration tests for retrofit services

---
+++ app/src/test/java/com/example/outdoorsy/WeatherRepositoryTest.kt
@@ -0,0 +1,63 @@
+package com.example.outdoorsy
+
+import com.example.outdoorsy.data.remote.WeatherApiService
+import com.example.outdoorsy.data.repository.WeatherRepositoryImpl
+import com.example.outdoorsy.di.ApiKeyInterceptor
+import com.example.outdoorsy.utils.BASE_URL
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import kotlinx.coroutines.test.runTest
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class WeatherRepositoryTest {
+    private lateinit var gson: Gson
+    private lateinit var repository: WeatherRepositoryImpl
+
+    @Before
+    fun setUp() {
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(ApiKeyInterceptor())
+            .addInterceptor(
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            )
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val weatherService = retrofit.create(WeatherApiService::class.java)
+        repository = WeatherRepositoryImpl(weatherService)
+
+        gson = GsonBuilder()
+            .setPrettyPrinting()
+            .create()
+    }
+
+    @Test
+    fun `test getCurrentWeatherByCity`() = runTest {
+        val result = repository.getCurrentWeatherByCity("Helsinki", "metric", "fi")
+        println("Success: ${gson.toJson(result)}")
+        assertTrue("City name should not be empty", result.name.isNotEmpty())
+    }
+
+    @Test
+    fun `test getCurrentWeatherByCoordinates`() = runTest {
+        val result = repository.getCurrentWeatherByCoordinates(
+            60.192059,
+            24.94583,
+            "metric",
+            "fi"
+        )
+        println("Success: ${gson.toJson(result)}")
+        assertTrue("City name should not be empty", result.name.isNotEmpty())
+    }
+}
