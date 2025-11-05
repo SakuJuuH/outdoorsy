12b9f41a46726099dcf531f699a38f248b6bda59
IsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-11-05 17:51:12 +0200

    Add selectedTime to ActivityUiState

--- app/src/main/java/com/example/outdoorsy/viewmodel/ActivityUiState.kt
+++ app/src/main/java/com/example/outdoorsy/viewmodel/ActivityUiState.kt
@@ -1,8 +1,11 @@
 package com.example.outdoorsy.viewmodel
 
+import java.time.LocalTime
+
 data class ActivityUiState(
     val locations: List<String> = emptyList(),
     val activities: List<String> = emptyList(),
     val selectedLocation: String = "",
-    val selectedActivity: String = ""
+    val selectedActivity: String = "",
+    val selectedTime: LocalTime = LocalTime.now()
 )
