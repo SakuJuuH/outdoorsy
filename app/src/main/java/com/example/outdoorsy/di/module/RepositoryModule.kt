1f31e600f565659062be6d60efda9fba2601b5ea
IsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-11-22 22:11:06 +0200

    Add ActivityRepository bind in RepositoryModule

--- app/src/main/java/com/example/outdoorsy/di/module/RepositoryModule.kt
+++ app/src/main/java/com/example/outdoorsy/di/module/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package com.example.outdoorsy.di.module
 
+import com.example.outdoorsy.data.repository.ActivityLogRepositoryImpl
 import com.example.outdoorsy.data.repository.EbayRepositoryImpl
 import com.example.outdoorsy.data.repository.ForecastRepositoryImpl
 import com.example.outdoorsy.data.repository.WeatherRepositoryImpl
+import com.example.outdoorsy.domain.repository.ActivityLogRepository
 import com.example.outdoorsy.domain.repository.EbayRepository
 import com.example.outdoorsy.domain.repository.ForecastRepository
 import com.example.outdoorsy.domain.repository.WeatherRepository
@@ -26,4 +28,7 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindEbayRepository(impl: EbayRepositoryImpl): EbayRepository
+
+    @Binds
+    abstract fun bindActivityLogRepository(impl: ActivityLogRepositoryImpl): ActivityLogRepository
 }
