632d7685cccbf96bab16611cc2b935fdd0b6d253
IsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-11-25 17:41:36 +0200

    Add toDomain method to ActivityEntity

--- app/src/main/java/com/example/outdoorsy/data/local/entity/ActivityEntity.kt
+++ app/src/main/java/com/example/outdoorsy/data/local/entity/ActivityEntity.kt
@@ -2,10 +2,13 @@ package com.example.outdoorsy.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.outdoorsy.domain.model.Activity
 
 @Entity(tableName = "activity")
 data class ActivityEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String
-)
+) {
+    fun toDomain(): Activity = Activity(name = name)
+}
