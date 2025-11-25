028d12ae612387fa83adb1816b07cdc782c7c855
IsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-11-25 17:43:13 +0200

    Add ActivityRepository binder to RepositoryModule

--- app/src/main/java/com/example/outdoorsy/di/module/RepositoryModule.kt
+++ app/src/main/java/com/example/outdoorsy/di/module/RepositoryModule.kt
@@ -1,10 +1,12 @@
 package com.example.outdoorsy.di.module
 
 import com.example.outdoorsy.data.repository.ActivityLogRepositoryImpl
+import com.example.outdoorsy.data.repository.ActivityRepositoryImpl
 import com.example.outdoorsy.data.repository.EbayRepositoryImpl
 import com.example.outdoorsy.data.repository.ForecastRepositoryImpl
 import com.example.outdoorsy.data.repository.WeatherRepositoryImpl
 import com.example.outdoorsy.domain.repository.ActivityLogRepository
+import com.example.outdoorsy.domain.repository.ActivityRepository
 import com.example.outdoorsy.domain.repository.EbayRepository
 import com.example.outdoorsy.domain.repository.ForecastRepository
 import com.example.outdoorsy.domain.repository.WeatherRepository
@@ -31,4 +33,7 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindActivityLogRepository(impl: ActivityLogRepositoryImpl): ActivityLogRepository
+
+    @Binds
+    abstract fun bindActivityRepository(impl: ActivityRepositoryImpl): ActivityRepository
 }
