4097f6eba8e134e854caa52af0e653b81fa9b126
IsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-11-25 17:42:28 +0200

    Add ActivityRepository interface

---
+++ app/src/main/java/com/example/outdoorsy/domain/repository/ActivityRepository.kt
@@ -0,0 +1,14 @@
+package com.example.outdoorsy.domain.repository
+
+import com.example.outdoorsy.domain.model.Activity
+import kotlinx.coroutines.flow.Flow
+
+interface ActivityRepository {
+    fun getAllActivities(): Flow<List<Activity>>
+
+    fun getActivityById(id: Int): Flow<Activity>
+
+    fun saveActivity(activity: Activity)
+
+    fun deleteActivity(activity: Activity)
+}
