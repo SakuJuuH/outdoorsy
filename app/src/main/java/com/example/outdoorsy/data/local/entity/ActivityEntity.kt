89f8acbfb02062404d424a7c6e1798a22dba80c3
IsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-11-26 11:12:35 +0200

    Update ActivityEntity to have a unique name parameter

--- app/src/main/java/com/example/outdoorsy/data/local/entity/ActivityEntity.kt
+++ app/src/main/java/com/example/outdoorsy/data/local/entity/ActivityEntity.kt
@@ -1,10 +1,14 @@
 package com.example.outdoorsy.data.local.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.example.outdoorsy.domain.model.Activity
 
-@Entity(tableName = "activity")
+@Entity(
+    tableName = "activity",
+    indices = [Index(value = ["name"], unique = true)]
+)
 data class ActivityEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
