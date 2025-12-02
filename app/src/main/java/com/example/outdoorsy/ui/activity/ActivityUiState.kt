9fd6cab9a0563624df5c0a2ced6fb158bb0a8bcf
IsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-12-02 21:31:44 +0200

    Add start and end date to Activity UI state

--- app/src/main/java/com/example/outdoorsy/ui/activity/ActivityUiState.kt
+++ app/src/main/java/com/example/outdoorsy/ui/activity/ActivityUiState.kt
@@ -5,14 +5,21 @@ import com.example.outdoorsy.domain.model.Activity
 import java.time.LocalTime
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+val timeNow: LocalDateTime = LocalDateTime.now()
+val timePlusOne: LocalDateTime = timeNow.plusHours(1)
 
 data class ActivityUiState(
     val locations: List<String> = emptyList(),
     val activities: Flow<List<Activity>> = flowOf(emptyList()),
     val selectedLocation: String? = null,
     val selectedActivity: Activity? = null,
-    val selectedStartTime: LocalTime = LocalTime.now(),
-    val selectedEndTime: LocalTime = LocalTime.now().plusHours(1),
+    val selectedStartTime: LocalTime = timeNow.toLocalTime(),
+    val selectedEndTime: LocalTime = timePlusOne.toLocalTime(),
+    val selectedStartDate: LocalDate = timeNow.toLocalDate(),
+    val selectedEndDate: LocalDate = timePlusOne.toLocalDate(),
     val timeRangeErrorId: Int? = null,
     // true = search successful, false = search error, null = search not performed
     val searchPerformed: Boolean? = null,
