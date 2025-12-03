a87e02d6728b684e6fa6beeed3e9542bbe20b476
IsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-12-03 11:12:49 +0200

    Update EditableFilteringInput corners to be round

--- app/src/main/java/com/example/outdoorsy/ui/activity/components/EditableFilteringInput.kt
+++ app/src/main/java/com/example/outdoorsy/ui/activity/components/EditableFilteringInput.kt
@@ -3,6 +3,7 @@ package com.example.outdoorsy.ui.activity.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
@@ -17,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -67,12 +69,14 @@ internal fun EditableFilteringInput(
                     },
                 trailingIcon = {
                     ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
-                }
+                },
+                shape = RoundedCornerShape(12.dp)
             )
 
             ExposedDropdownMenu(
                 expanded = expanded && (text.isNotBlank() || filteredOptions.isNotEmpty()),
-                onDismissRequest = { expanded = false }
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.clip(RoundedCornerShape(12.dp))
             ) {
                 if (limitedOptions.isEmpty()) {
                     DropdownMenuItem(
